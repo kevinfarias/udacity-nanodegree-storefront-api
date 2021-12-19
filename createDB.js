b58617acb69ef5ef27b89715278dcd2de2f46c71
Kevin Farias <kevin.farias.dev@gmail.com>
2021-12-19 15:17:33 -0300

    changing createDB method

---
+++ createDB.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import pg from "pg";
+
+dotenv.config();
+
+if (!process.env.ENV) {
+  process.env.ENV = "dev";
+}
+
+const conn = new pg.Pool({
+  connectionString: `postgres://${process.env.DB_USER}:${
+    process.env.DB_PASSWORD
+  }@${process.env.DB_HOST}:${process.env.DB_PORT}/postgres`,
+});
+
+const conn = await Client.connect();
+const sql = `CREATE DATABASE ${process.env.DB_NAME}`;
+
+await conn.query(sql);
+
+conn.release();
